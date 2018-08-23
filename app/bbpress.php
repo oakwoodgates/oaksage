1a5833f4ab50378222be4f583f794fb36d53a60d
WPGuru4u <wpguru4u@gmail.com>
2018-08-23 14:58:58 -0400

    bbpress support

--- app/bbpress.php
+++ app/bbpress.php
@@ -13,3 +13,9 @@ function oaksage_bbp_get_template_part( $templates, $slug, $name = null ) {
 	return $templates;
 }
 add_filter( 'bbp_get_template_part', 'oaksage_bbp_get_template_part', 10, 3 );
+
+function custom_bbp_sub_forum_list( $args ) {
+	$args['separator'] = '<br/>';
+	return $args;
+}
+ add_filter('bbp_after_list_forums_parse_args', 'custom_bbp_sub_forum_list' );
